53f40c19616ecd8157f1c66d8eb1147e83a18854
devngho <yjh135908@gmail.com>
2024-10-20 20:06:50 +0900

    opt

--- MaxText/input_pipeline/_hf_data_processing.py
+++ MaxText/input_pipeline/_hf_data_processing.py
@@ -138,7 +138,7 @@ def preprocessing_pipeline(
       operations=operations,
       sampler=index_sampler,
       worker_count=num_threads if random_access else 1,  # only supports one worker for now, more workers results in duplicated data
-      worker_buffer_size=1 if random_access else 1,
+      worker_buffer_size=128 if random_access else 1,
       read_options=grain.ReadOptions(num_threads=num_threads, prefetch_buffer_size=128),
   )
 
